b31e129ab1084c35e06fa0e00904163e31a15e5b
Kristina Faye <hellokristinafaye@gmail.com>
2025-06-06 14:59:58 -0700

    headshot updated

--- src/components/Hero/Hero.jsx
+++ src/components/Hero/Hero.jsx
@@ -1,11 +1,11 @@
 import React from 'react'
 import './Hero.css';
-import profile_img from '../../assets/profile_img.svg'
+import headshotLGSquare from '../../assets/headshotLGSquare.jpg'
 
 const Hero = () => {
   return (
     <div className="hero">
-          <img src={profile_img} alt="" className="" />      
+          <img src={headshotLGSquare} alt="" className="" />      
           <h1 className="">I'm Kristina Glinoga, Full Stack Developer based in the USA.</h1>
           <p className="">I am a Full Stack Developer from Washington State, open to new work! </p>
           <div className="hero-action">
