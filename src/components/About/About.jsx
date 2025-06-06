d7faed4e89063ea7d53532f8d596c472ade7e35d
Kristina Faye <hellokristinafaye@gmail.com>
2025-06-06 16:46:38 -0700

    theme pattern imported

--- src/components/About/About.jsx
+++ src/components/About/About.jsx
@@ -1,9 +1,15 @@
 import React from 'react'
 import './About.css'
+import theme_pattern from '../../assets/theme_pattern.svg'
 
 const About = () => {
   return (
-    <div className="about">About</div>
+      <div className="about">
+          <div className="about-title">
+              <h1 className="">About Me</h1>
+              <img src={theme_pattern} alt="" className="" />
+          </div>
+    </div>
   )
 }
 
