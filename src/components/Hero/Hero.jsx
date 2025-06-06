df2bb06750b3df881fe540cfb02ee19cdfb68be4
Kristina Faye <hellokristinafaye@gmail.com>
2025-06-06 14:53:44 -0700

    text content added

--- src/components/Hero/Hero.jsx
+++ src/components/Hero/Hero.jsx
@@ -5,7 +5,9 @@ import profile_img from '../../assets/profile_img.svg'
 const Hero = () => {
   return (
     <div className="hero">
-        <img src={profile_img} alt="" className="" />      
+          <img src={profile_img} alt="" className="" />      
+          <h1 className="">I'm Kristina Glinoga, Full Stack Developer based in the USA.</h1>
+          <p className="">I am a Full Stack Developer from Washington State, open to new work! </p>
     </div>
   )
 }
