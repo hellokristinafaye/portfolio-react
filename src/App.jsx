50354186851d51c74ad616ec4e05d402bdbe1052
Kristina Faye <hellokristinafaye@gmail.com>
2025-06-06 16:43:25 -0700

    About component mounted

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,14 @@
 import React from 'react'
 import Navbar from './components/NavBar/Navbar'
 import Hero from './components/Hero/Hero'
+import About from './components/About/About'
 
 const App = () => {
   return (
     <div>
       <Navbar />
       <Hero />
+      <About />
     </div>
   )
 }
