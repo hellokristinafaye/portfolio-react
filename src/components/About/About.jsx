010f6c5481dffc8c873587a1e24cb676911c11e0
Kristina Faye <hellokristinafaye@gmail.com>
2025-06-06 16:49:03 -0700

    headshot added

--- src/components/About/About.jsx
+++ src/components/About/About.jsx
@@ -1,14 +1,23 @@
 import React from 'react'
 import './About.css'
 import theme_pattern from '../../assets/theme_pattern.svg'
+import headshotLGSquare from '../../assets/headshotLGSquare.jpg'
 
 const About = () => {
   return (
       <div className="about">
+          
           <div className="about-title">
               <h1 className="">About Me</h1>
               <img src={theme_pattern} alt="" className="" />
           </div>
+
+          <div className="about-sections">
+              <div className="about-left">
+                  <img src={headshotLGSquare} alt="" className="" />
+              </div>
+          </div>
+
     </div>
   )
 }
