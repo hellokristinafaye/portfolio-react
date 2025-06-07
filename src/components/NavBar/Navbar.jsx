18bf58b8d86e14d91f547206ec7b6ab8bcd96569
Kristina Faye <hellokristinafaye@gmail.com>
2025-06-06 20:55:46 -0700

    p tag added to navbar

--- src/components/NavBar/Navbar.jsx
+++ src/components/NavBar/Navbar.jsx
@@ -4,17 +4,25 @@ import logo from '../../assets/logo.svg'
 
 const Navbar = () => {
   return (
-      <div className="navbar">
-          <img src={logo} alt="" className="" />
-          <ul className="nav-menu">
-              <li className="">Home</li>
-              <li className="">About Me</li>
-              <li className="">Portfolio</li>
-              <li className="">Contact</li>
-          </ul>
-          <div className="nav-connect">Connect With Me</div>
-      </div>
-  )
+    <div className="navbar">
+      <img src={logo} alt="" className="" />
+      <ul className="nav-menu">
+        <li className="">
+          <p onClick={() => "home"} className="">Home</p>
+        </li>
+        <li className="">
+          <p onClick={() => "about"} className="">About Me</p>
+        </li>
+        <li className="">
+          <p onClick={() => "work"} className="">Portfolio</p>
+        </li>
+        <li className="">
+          <p onClick={() => "contactg"} className="">Contact</p>
+        </li>
+      </ul>
+      <div className="nav-connect">Connect With Me</div>
+    </div>
+  );
 }
 
 export default Navbar
