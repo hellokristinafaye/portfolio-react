e2fec9e99a31cc082a74bae73f16f867c3ac02e2
Kristina Faye <hellokristinafaye@gmail.com>
2025-06-07 12:20:26 -0700

    OK states and conditional work for ONE link

--- src/components/NavBar/Navbar.jsx
+++ src/components/NavBar/Navbar.jsx
@@ -1,41 +1,52 @@
-import React from 'react'
+import React, { useState } from 'react'
 import './Navbar.css'
 import logo from '../../assets/logo.svg'
-
+import underline from '../../assets/nav_underline.svg'
 
 
 const Navbar = () => {
+
+    const [menu, setMenu] = useState("home");
+
   return (
-    <div className="navbar">
+      <div className="navbar">
+          
       <img src={logo} alt="" className="" />
       <ul className="nav-menu">
         <li className="">
-          <AnchorLink className="anchor-link">
+          {/* <AnchorLink className="anchor-link" href="#home"> */}
             <p onClick={() => setMenu("home")} className="">
               Home
             </p>
-          </AnchorLink>
+            {menu === "home" ? <img src={underline} /> : <></>}
+          {/* </AnchorLink> */}
         </li>
+
         <li className="">
-          <AnchorLink className="anchor-link">
+          {/* <AnchorLink className="anchor-link" offset={50} href="#about"> */}
             <p onClick={() => setMenu("about")} className="">
               About Me
             </p>
-          </AnchorLink>
+            {/* {menu === "about" ? <img src={underline} /> : <></>} */}
+          {/* </AnchorLink> */}
         </li>
+
         <li className="">
-          <AnchorLink className="anchor-link">
+          {/* <AnchorLink className="anchor-link" offset={50} href="#work"> */}
             <p onClick={() => setMenu("work")} className="">
               Portfolio
             </p>
-          </AnchorLink>
+            {/* {menu === "work" ? <img src={underline} /> : <></>} */}
+          {/* </AnchorLink> */}
         </li>
+
         <li className="">
-          <AnchorLink className="anchor-link">
+          {/* <AnchorLink className="anchor-link" offset={50} href="#contact"> */}
             <p onClick={() => setMenu("contact")} className="">
               Contact
             </p>
-          </AnchorLink>
+            {/* {menu === "contact" ? <img src={underline} /> : <></>} */}
+          {/* </AnchorLink> */}
         </li>
       </ul>
       <div className="nav-connect">Connect With Me</div>
