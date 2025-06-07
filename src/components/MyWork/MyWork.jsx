22bd59c55ce347d1bddeb7a8fb5804cd4fd8aca1
Kristina Faye <hellokristinafaye@gmail.com>
2025-06-06 18:00:46 -0700

    arrow icon added

--- src/components/MyWork/MyWork.jsx
+++ src/components/MyWork/MyWork.jsx
@@ -2,6 +2,7 @@ import React from 'react'
 import './MyWork.css'
 import theme_pattern from '../../assets/theme_pattern.svg'
 import mywork_data from '../../assets/mywork_data'
+import arrow_icon from '../../assets/arrow_icon.svg'
 
 const MyWork = () => {
   return (
@@ -11,11 +12,16 @@ const MyWork = () => {
         <img src={theme_pattern} alt="" className="" />
       </div>
 
-          <div className="mywork-container">
-              {mywork_data.map((work, index) => {
-                  return <img key={index} src={work.w_img} alt="" className="" />
-              })}
-          </div>
+      <div className="mywork-container">
+        {mywork_data.map((work, index) => {
+          return <img key={index} src={work.w_img} alt="" className="" />;
+        })}
+      </div>
+
+      <div className="mywork-showmore">
+        <p className="">Show More</p>
+        <img src={arrow_icon} alt="" className="" />
+      </div>
     </div>
   );
 }
