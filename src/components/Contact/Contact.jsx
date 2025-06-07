695ddfbe49527ffa4d0cbba2ed1392f9c4d75ff6
Kristina Faye <hellokristinafaye@gmail.com>
2025-06-06 18:27:48 -0700

    theme pattern imported and used

--- src/components/Contact/Contact.jsx
+++ src/components/Contact/Contact.jsx
@@ -1,10 +1,16 @@
 import React from 'react'
 import './Contact.css'
+import theme_pattern from '../../assets/theme_pattern.svg'
 
 const Contact = () => {
   return (
-    <div>Contact</div>
-  )
+    <div className="contact">
+      <div className="contact-title">
+        <h1 className="">Get In Touch</h1>
+        <img src={theme_pattern} alt="" className="" />
+      </div>
+    </div>
+  );
 }
 
 export default Contact
