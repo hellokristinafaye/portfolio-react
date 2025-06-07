97f679c5637711c57408915472e56bffbaf1cf13
Kristina Faye <hellokristinafaye@gmail.com>
2025-06-06 20:50:55 -0700

    smoothscroll for about

--- src/components/About/About.jsx
+++ src/components/About/About.jsx
@@ -5,7 +5,7 @@ import headShotRectangle from "../../assets/headShotRectangle.jpg";
 
 const About = () => {
   return (
-    <div  className="about">
+    <div id="about" className="about">
       <div className="about-title">
         <h1 className="">About Me</h1>
         <img src={theme_pattern} alt="" className="" />
