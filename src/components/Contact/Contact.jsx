a6b3659bb8b35cd849260f3a357a37e4c8da10c1
Kristina Faye <hellokristinafaye@gmail.com>
2025-06-06 18:41:48 -0700

    fixed a div

--- src/components/Contact/Contact.jsx
+++ src/components/Contact/Contact.jsx
@@ -10,37 +10,59 @@ const Contact = () => {
       <div className="contact-title">
         <h1 className="">Get In Touch</h1>
         <img src={theme_pattern} alt="" className="" />
-
-        <div className="contact-section">
-          <div className="contact-left">
-            <h1 className="">Let's talk</h1>
-            <p className="">
-              I'm currently open to new work so please reach out if you're
-              looking for a remote Front End or Full Stack Web Developer.
-            </p>
-            <div className="contact-details">
-              <div className="contact-detail">
-                <img src={mail_icon} alt="" className="" />
-                <p className="">helloKristinaFaye@gmail.com</p>
-              </div>
-              <div className="contact-detail">
-                <img src={location_icon} alt="" className="" />
-                <p className="">Seattle, Washington, USA</p>
-              </div>
+      </div>
+      <div className="contact-section">
+        <div className="contact-left">
+          <h1 className="">Let's talk</h1>
+          <p className="">
+            I'm currently open to new work so please reach out if you're looking
+            for a remote Front End or Full Stack Web Developer.
+          </p>
+          <div className="contact-details">
+            <div className="contact-detail">
+              <img src={mail_icon} alt="" className="" />
+              <p className="">helloKristinaFaye@gmail.com</p>
+            </div>
+            <div className="contact-detail">
+              <img src={location_icon} alt="" className="" />
+              <p className="">Seattle, Washington, USA</p>
             </div>
           </div>
+        </div>
 
-                  <div className="contact-right">
-                      <form className="contact-right">
-                          <label htmlFor="" className="">Your Name</label>
-                          <input type="text" placeholder="Enter your name" name='name' className="" />
-                          <label htmlFor="" className="">Your Email</label>
-                          <input type="email" placeholder="Enter your email address" name='email' className="" />
-                          <label htmlFor="" className="">Write your message here</label>
-                          <textarea name="message" rows="8" placeholder="Enter your message" className=""></textarea>
-                          <button type="submit" className="contact-submit">Submit Now</button>
-                      </form>
-          </div>
+        <div className="contact-right">
+          <form className="contact-right">
+            <label htmlFor="" className="">
+              Your Name
+            </label>
+            <input
+              type="text"
+              placeholder="Enter your name"
+              name="name"
+              className=""
+            />
+            <label htmlFor="" className="">
+              Your Email
+            </label>
+            <input
+              type="email"
+              placeholder="Enter your email address"
+              name="email"
+              className=""
+            />
+            <label htmlFor="" className="">
+              Write your message here
+            </label>
+            <textarea
+              name="message"
+              rows="8"
+              placeholder="Enter your message"
+              className=""
+            ></textarea>
+            <button type="submit" className="contact-submit">
+              Submit Now
+            </button>
+          </form>
         </div>
       </div>
     </div>
