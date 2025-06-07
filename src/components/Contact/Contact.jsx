4e34fa19ee7089e926ab88ed850a6895a7de3554
Kristina Faye <hellokristinafaye@gmail.com>
2025-06-07 12:41:21 -0700

    web3form good!

--- src/components/Contact/Contact.jsx
+++ src/components/Contact/Contact.jsx
@@ -5,6 +5,34 @@ import mail_icon from '../../assets/mail_icon.svg';
 import location_icon from '../../assets/location_icon.svg';
 
 const Contact = () => {
+// web3form code start
+const [result, setResult] = React.useState("");
+
+const onSubmit = async (event) => {
+  event.preventDefault();
+  setResult("Sending....");
+  const formData = new FormData(event.target);
+  
+  formData.append("access_key", "3ce93469-269d-4b12-a1cb-ba66c92a0ec2");
+  
+  const response = await fetch("https://api.web3forms.com/submit", {
+    method: "POST",
+    body: formData,
+  });
+  
+  const data = await response.json();
+  // web3form code end
+  
+  if (data.success) {
+    setResult("Form Submitted Successfully");
+    event.target.reset();
+  } else {
+    console.log("Error", data);
+    setResult(data.message);
+  }
+};
+
+
   return (
     <div id="contact" className="contact">
       <div className="contact-title">
@@ -31,7 +59,7 @@ const Contact = () => {
         </div>
 
         <div className="contact-right">
-          <form className="contact-right">
+          <form onSubmit={onSubmit} className="contact-right">
             <label htmlFor="" className="">
               Your Name
             </label>
