732543020f7ea440404f35f53ffcc449724f8b99
Kristina Faye <hellokristinafaye@gmail.com>
2025-06-06 17:56:33 -0700

    theme pattern imported and used

--- src/components/MyWork/MyWork.jsx
+++ src/components/MyWork/MyWork.jsx
@@ -1,12 +1,16 @@
 import React from 'react'
 import './MyWork.css'
+import theme_pattern from '../../assets/theme_pattern.svg'
 
 const MyWork = () => {
   return (
-      <div className="mywork">
-        
+    <div className="mywork">
+      <div className="mywork-title">
+        <h1 className="">My Latest Work</h1>
+        <img src={theme_pattern} alt="" className="" />
+      </div>
     </div>
-  )
+  );
 }
 
 export default MyWork
