b1865e97e3f75f799914a8fb8aef869faa2ae53d
Kristina Faye <hellokristinafaye@gmail.com>
2025-06-06 21:06:49 -0700

    class added

--- src/components/NavBar/Navbar.jsx
+++ src/components/NavBar/Navbar.jsx
@@ -2,34 +2,36 @@ import React from 'react'
 import './Navbar.css'
 import logo from '../../assets/logo.svg'
 
+
+
 const Navbar = () => {
   return (
     <div className="navbar">
       <img src={logo} alt="" className="" />
       <ul className="nav-menu">
         <li className="">
-          <AnchorLink>
+          <AnchorLink className="anchor-link">
             <p onClick={() => setMenu("home")} className="">
               Home
             </p>
           </AnchorLink>
         </li>
         <li className="">
-          <AnchorLink>
+          <AnchorLink className="anchor-link">
             <p onClick={() => setMenu("about")} className="">
               About Me
             </p>
           </AnchorLink>
         </li>
         <li className="">
-          <AnchorLink>
+          <AnchorLink className="anchor-link">
             <p onClick={() => setMenu("work")} className="">
               Portfolio
             </p>
           </AnchorLink>
         </li>
         <li className="">
-          <AnchorLink>
+          <AnchorLink className="anchor-link">
             <p onClick={() => setMenu("contact")} className="">
               Contact
             </p>
