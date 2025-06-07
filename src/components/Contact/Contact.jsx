8b6ae32720a8258aa419df9c6f7d62f8a8575cd9
Kristina Faye <hellokristinafaye@gmail.com>
2025-06-06 18:28:59 -0700

    title added

--- src/components/Contact/Contact.jsx
+++ src/components/Contact/Contact.jsx
@@ -1,6 +1,6 @@
-import React from 'react'
-import './Contact.css'
-import theme_pattern from '../../assets/theme_pattern.svg'
+import React from "react";
+import "./Contact.css";
+import theme_pattern from "../../assets/theme_pattern.svg";
 
 const Contact = () => {
   return (
@@ -8,9 +8,18 @@ const Contact = () => {
       <div className="contact-title">
         <h1 className="">Get In Touch</h1>
         <img src={theme_pattern} alt="" className="" />
+
+              <div className="contact-section">
+                  
+                  <div className="contact-left">
+                      <h1 className="">Let's talk</h1>
+          </div>
+          
+          <div className="contact-right"></div>
+        </div>
       </div>
     </div>
   );
-}
+};
 
-export default Contact
+export default Contact;
