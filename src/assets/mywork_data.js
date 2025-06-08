4420f86cbe7066c1a9b55820fa18cb9bae11f1f7
Kristina Faye <hellokristinafaye@gmail.com>
2025-06-08 16:33:20 -0700

    project names updated

--- src/assets/mywork_data.js
+++ src/assets/mywork_data.js
@@ -8,32 +8,32 @@ import project6_img from '../assets/project_6.svg'
 const mywork_data = [
     {
         w_no:1,
-        w_name:"Web design",
+        w_name:"Grocery E-commerce",
         w_img:projectGreenCartPic
     },
     {
         w_no:2,
-        w_name:"Web design",
+        w_name:"Netflix Clone",
         w_img:projectNetflixClone
     },
     {
         w_no:3,
-        w_name:"Web design",
+        w_name:"AirBnb Clone",
         w_img:projectStayBnB
     },
     {
         w_no:4,
-        w_name:"Web design",
+        w_name:"Weather App with Dark Mode",
         w_img:projectWeatherApp
     },
     {
         w_no:5,
-        w_name:"Web design",
+        w_name:"To Do List",
         w_img:projectToDoList
     },
     {
         w_no:6,
-        w_name:"Web design",
+        w_name:"Placeholder",
         w_img:project6_img
     },
 ]
