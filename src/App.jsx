0b7f6ca4b9bac8e386e517a2fe291f9097e9e712
Kristina Faye <hellokristinafaye@gmail.com>
2025-06-08 15:44:01 -0700

    moving on to projects

--- src/App.jsx
+++ src/App.jsx
@@ -15,7 +15,6 @@ const App = () => {
       <MyWork />
       {/* next steps: upload your own work for the portfolio area.  Needs screenshots for everything */}
       <Contact />
-    {/* next steps: add GitHub link to Contact */}
       <Footer />
       {/* wanted to add a scroll to top button, will wait for future dev */}
       {/* <button className="to-top">^</button> */}
