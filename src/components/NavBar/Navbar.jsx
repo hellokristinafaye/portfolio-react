e2f3b785a09e250179b54fdcdccf7a119f3f325d
Kristina Faye <hellokristinafaye@gmail.com>
2025-06-08 19:08:37 -0700

    smooth scroll better centered

--- src/components/NavBar/Navbar.jsx
+++ src/components/NavBar/Navbar.jsx
@@ -45,7 +45,7 @@ const Navbar = () => {
         </li>
 
         <li className="">
-          <AnchorLink className="anchor-link" offset={100} href="#work">
+          <AnchorLink className="anchor-link" offset={50} href="#work">
             <p onClick={() => setMenu("work")} className="">
               Portfolio
             </p>
