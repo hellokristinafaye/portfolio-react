5fcd22c6bcca2bad5432d8b1ff3861fa280a1dcd
Kristina Faye <hellokristinafaye@gmail.com>
2025-06-11 13:07:25 -0700

    label added to tech desc

--- src/components/MyWork/MyWork.jsx
+++ src/components/MyWork/MyWork.jsx
@@ -24,7 +24,7 @@ const MyWork = () => {
                   <div className="work-overlay">
                     <p className="">{work.w_name}</p>
                     <p className="work-desc">{work.w_desc}</p>
-                    <p className="work-desc">{work.w_tech}</p>
+                    <p className="work-desc">Tech Used: {work.w_tech}</p>
 
                     <a href={work.w_deploy_link} target="_blank" className="">
                       Live Demo
